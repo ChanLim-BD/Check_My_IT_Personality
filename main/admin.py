db8a47bec135921d21e1a7e8d53c4beb670ad661
Chan <linsay070@gmail.com>
2025-01-18 20:11:23 +0900

    admin 설정

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Developer, Question, Choice
 
-# Register your models here.
+@admin.register(Developer)
+class DeveloperAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    pass
