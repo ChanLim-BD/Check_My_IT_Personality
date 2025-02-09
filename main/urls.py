d462dec8d13984607e453b6b6d05301c1769c9b0
Chan <linsay070@gmail.com>
2025-02-09 22:46:53 +0900

    메인페이지 리다이렉팅 설정 추가

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . import views
+from main import views
+
+app_name="main"
 
 urlpatterns =[
-    path('', views.index),
-    path('form/', views.form),
-    path('submit/', views.submit),
-    path('result/<int:developer_id>/', views.result),
+    path('', views.index, name="index"),
+    path('form/', views.form, name="form"),
+    path('submit/', views.submit, name="submit"),
+    path('result/<int:developer_id>/', views.result, name="result"),
 ]
